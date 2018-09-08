c8ad1434562678c36a41459b2e36754a36302528
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-09-08 12:17:09 +0530

    Create pl.java

---
+++ pl.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+String input = "aabbcc"; 
+  
+        StringBuilder a = new StringBuilder(); 
+  
+        
+        a.append(input); 
+  
+         
+        a = a.reverse(); 
+  
+        
+        System.out.println(a); 
+    }
+	}
